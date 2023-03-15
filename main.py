31472c86490da606f1f7ac556b8c847ba201ff37
Larbi Gharib <larbizard@gmail.com>
2023-03-15 10:42:53 +0100

    Add send push notifications

--- main.py
+++ main.py
@@ -7,14 +7,48 @@ import argparse
 import emission.net.ext_service.push.notify_usage as pnu
 import uuid
 import pytz
+import requests
 
 
 def fetch_users(db, project):
-    return db.Stage_Profiles.find({"project_name": {"$in": [project["project_name"]["english"], project["project_name"]["french"]]}})
+    return db.Stage_Profiles.find({"project_name": {"$in": [project["name_fr"], project["name_en"]]}})
+
+def getTitleBodyTuple(notification, language):
+    title = ""
+    body = ""
+    for element in notification["titles"]:
+        if element["language"] == language:
+            title = element["title"]
+    for element in notification["messages"]:
+        if element["language"] == language:
+            body = element["body"]
+    return (title, body)
+
+def sendPushNotifications(timezone, user, delta, daily_notifications):
+    try:
+        for notification in daily_notifications:
+            if notification["day"] == str(delta.days):
+                if (datetime.now(timezone).hour - int(notification["display_time"][0:2])) == 0:
+
+                    (title, message) = getTitleBodyTuple(notification, user["phone_lang"])
+
+                    json_data = {
+                        "title": title,
+                        "message": message
+                    }
+                    response = pnu.send_visible_notification_to_users([user["user_id"]], title, message, json_data, False)
+                    print(response)
+                elif datetime.now().hours() - notification["hour"] > 0:
+                    print("Notification already sent or cronjob was missed")
+                else:
+                    print("It's not time to send the notification")
+    except:
+        pass
+
 
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument("-p", "--projectconfigfile", required=True, help="Path to project notifications config json file")
+    ap.add_argument("-p", "--projectconfigendpoint", required=True, help="Path to project notifications config json file")
     args = vars(ap.parse_args())
     
 
@@ -24,33 +58,18 @@ def main():
     client = MongoClient(env["timeseries"]["url"])
     db = client.Stage_database
 
-    with open(args["projectconfigfile"]) as project_file:
-        project = json.load(project_file)
-        users = fetch_users(db, project)
+    response = requests.get(args["projectconfigendpoint"])
 
-        timezone = pytz.timezone(project["timezone"])
-        
-        for user in users:
+    project = json.load(response)
+    users = fetch_users(db, project)
 
-            delta = datetime.now()-user["update_ts"]
+    timezone = pytz.timezone(project["timezone"])
+    
+    for user in users:
 
-            try:
-                for notification in project["notifications_day"][str(delta.days)]["notifications"]:
-                    if (datetime.now(timezone).hour - int(notification["hour"])) == 0:
-                        title = notification["title_fr"] if user["phone_lang"] == "fr" else notification["title_en"]
-                        message = notification["message_fr"] if user["phone_lang"] == "fr" else notification["message_en"] 
-                        json_data = {
-                            "title": title,
-                            "message": message
-                        }
-                        response = pnu.send_visible_notification_to_users([user["user_id"]], title, message, json_data, False)
-                        print(response)
-                    elif datetime.now().hours() - notification["hour"] > 0:
-                        print("Notification already sent or cronjob was missed")
-                    else:
-                        print("It's not time to send the notification")
-            except:
-                pass
+        delta = datetime.now()-user["update_ts"]
+
+        sendPushNotifications(timezone, user, delta, project["daily_notifications"])
 
         
 
